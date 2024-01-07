1b85a5a263a914c11f41cab66ec000bf1efe1a2d
satyam-kr03 <b22236@students.iitmandi.ac.in>
2024-01-07 18:33:14 +0530

    added HomeRoute

--- lib/presentation/routes/router.g.dart
+++ lib/presentation/routes/router.g.dart
@@ -9,6 +9,7 @@ part of 'router.dart';
 List<RouteBase> get $appRoutes => [
       $splashRoute,
       $loginRoute,
+      $homeRoute,
     ];
 
 RouteBase get $splashRoute => GoRouteData.$route(
@@ -43,6 +44,28 @@ extension $LoginRouteExtension on LoginRoute {
 
   String get location => GoRouteData.$location(
         '/login',
+      );
+
+  void go(BuildContext context) => context.go(location);
+
+  Future<T?> push<T>(BuildContext context) => context.push<T>(location);
+
+  void pushReplacement(BuildContext context) =>
+      context.pushReplacement(location);
+
+  void replace(BuildContext context) => context.replace(location);
+}
+
+RouteBase get $homeRoute => GoRouteData.$route(
+      path: '/home',
+      factory: $HomeRouteExtension._fromState,
+    );
+
+extension $HomeRouteExtension on HomeRoute {
+  static HomeRoute _fromState(GoRouterState state) => const HomeRoute();
+
+  String get location => GoRouteData.$location(
+        '/home',
       );
 
   void go(BuildContext context) => context.go(location);
